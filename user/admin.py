bb403751b090af62cb2f67349617287598e71c8d
Yoon joonyeol <raoneli@naver.com>
2023-04-05 22:47:19 +0900

    2nd commit 회원가입,로그인기능 완료

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,7 @@
+# django에서 admin툴을 사용
 from django.contrib import admin
+# 생성한 모델 가져오기
+from .models import UserModel
 
 # Register your models here.
+admin.site.register(UserModel) # 이 코드가 나의 UserModel을 Admin에 추가 해 줍니다
